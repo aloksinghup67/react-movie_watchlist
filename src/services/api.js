ef1225c27e9e895fc1650f562c0800eafbd910c1
aloksinghup67 <aloksinghkh43@gmail.com>
2025-03-19 20:42:27 +0530

    Update api.js
    filter adult movies

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,10 @@ const BASE_URL = "https://api.themoviedb.org/3";
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
-    return data.results;
+    
+    return data.results.filter(movie => !movie.adult);
 };
+
 
 export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
